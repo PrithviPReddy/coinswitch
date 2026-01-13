a3e0ecefd7f48527a9f11b7cace2e2037233fe85
Prithvi Reddy <prithvireddy0770@gmail.com>
2026-01-13 07:31:05 +0000

    added price API

--- app/api/tokens/route.ts
+++ app/api/tokens/route.ts
@@ -1,18 +1,18 @@
 import { NextRequest } from "next/server";
-import { getAssociatedTokenAddress } from "@solana/spl-token"
-import { SUPPORTED_TOKENS } from "@/app/lib/constants";
+import { getAccount, getAssociatedTokenAddress } from "@solana/spl-token"
+import { connection, SUPPORTED_TOKENS } from "@/app/lib/constants";
 import { PublicKey } from "@solana/web3.js";
 
 export async function GET(req:NextRequest, res:NextRequest){
     const { searchParams } = new URL(req.url)
-    const address = searchParams.get('address')!
+    const address = searchParams.get('address')!()
     const owner = new PublicKey(address)
     const balances = await Promise.all(
         SUPPORTED_TOKENS.map(token => {
             const mint = new PublicKey(token.mint);
             return getAssociatedTokenAddress(mint, owner);
-  })
-);
+        })
+    );
     //ATA  = Associated token account 
     //PDA  = PRogram derived address
 
@@ -20,9 +20,13 @@ export async function GET(req:NextRequest, res:NextRequest){
 
 }
 
-function getAccountBalance(token : { 
+async function getAccountBalance(token : { 
     name: string,
     mint : string
 }, address : string){
+
+    const ata = await getAssociatedTokenAddress(new PublicKey(token.mint),new PublicKey(address))
+    const account = await getAccount(connection,ata)
+
 
 }
--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,14 +1,50 @@
+import { getMinimumBalanceForRentExemptAccountWithExtensions } from "@solana/spl-token"
 import { Connection } from "@solana/web3.js"
+
+let LAST_UPDATED : number | null = null
+let prices: {[key: string]: {
+    price: string;
+}} = {};
+const TOKEN_PRICE_REFRESH_RATE = 60000
 
 export const SUPPORTED_TOKENS : {
     name : string,
-    mint : string
+    mint : string,
+    native :boolean
 }[] = [{
     name :"USDC",
-    mint:"EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+    mint:"EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+    native : false,
 },{
     name : "USDT",
-    mint : "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"
+    mint : "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
+    native : false,
+},{
+    name : "SOL",
+    mint : "So11111111111111111111111111111111111111112",
+    native : false,
 }]
 
 export const connection = new Connection("https://api.mainnet-beta.solana.com")
+
+export async function getSupportedTokens(){
+    const apiKey = process.env.JUP_API_KEY
+    if (!LAST_UPDATED || new Date().getTime() - LAST_UPDATED < TOKEN_PRICE_REFRESH_RATE){
+        const response = await (
+            await fetch(
+                'https://api.jup.ag/price/v3?ids=So11111111111111111111111111111111111111112',
+                {
+                    headers: {
+                        'x-api-key': apiKey!,
+                    },
+                }
+            )
+        ).json();
+        LAST_UPDATED = new Date().getTime();
+        prices = response.data.data
+    }
+     return SUPPORTED_TOKENS.map(s => ({
+        ...s,
+        price: prices[s.name].price
+    }))
+}
